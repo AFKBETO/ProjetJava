d9891d00c98a7ca97ea938353a18a17efee5aa40
AFKBETO <afk.beto@gmail.com>
2021-11-18 09:29:59 +0100

    Revert "Merge branch 'V2Viet' into main"
    This reverts commit ed2bbdb18268d7ef0800f15027b6b5b3afbbc1e7.

--- ProjetJeu/src/V1/Phase1.java
+++ ProjetJeu/src/V1/Phase1.java
@@ -1,15 +1,9 @@
 package V1;
 
-import java.util.ArrayList;
-
 public class Phase1 implements Phase {
-    public void selectionJoueur(ArrayList<Joueur> j4) {
-        for (Joueur joueur : j4) {
-            joueur.updateEtat("sélectionné");
-        }
-
+    public void selectionJoueur() {
+    	
     }
-
     public void deroulerPhaseJeu() {
     	
     }
